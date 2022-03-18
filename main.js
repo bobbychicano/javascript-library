580234da5daa04c17c3146f2fe1179c2ebaa559d
Robert <rescobedo12@gmail.com>
2022-03-18 15:05:33 -0700

    Write a rough outline of code logic in main.js and add dummy content to index.html as well as a minor style reset in styles.css.

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 let books = [];
 
+// when someon submits the form with the inputs that correspond the parameters below
+// the constructor needs to run in order to make a new Book object
 function Book(title, author, pages, read){
   this.title = title;
   this.author = author;
@@ -9,7 +11,16 @@ function Book(title, author, pages, read){
 
 function saveInput() {
   // save user input AS AN OBJECT, probably from a form, and add it into the book array
-  // will need array methods
-
-  books.push(object);
+  // the newBookVariable is where we save our new Book object after a user inputs arguments
+  books.push(newBookVariable);
 }
+
+// function that loops through the array and displays each book on the homepage
+function displayArray(booksArray) {
+  booksArray.forEach(() => para.textContent = array[index#])
+}
+
+// displaying each book in their own card would likely require grid-template-columns with repeat(autofit,
+// minmax())
+
+// Book.prototype.function = ""
