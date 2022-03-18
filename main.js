67d7816652d7719db561b3122e6118b4723a2fb8
Robert <rescobedo12@gmail.com>
2022-03-18 14:25:40 -0700

    Set up the initial HTML and JavaScript content.

---
+++ main.js
@@ -0,0 +1,15 @@
+let books = [];
+
+function Book(title, author, pages, read){
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
+}
+
+function saveInput() {
+  // save user input AS AN OBJECT, probably from a form, and add it into the book array
+  // will need array methods
+
+  books.push(object);
+}
