0c00b1a27023b16b49a022d406886d832c21dd3d
Robert <rescobedo12@gmail.com>
2022-03-20 16:09:29 -0700

    Apply basic styling to the form element.

--- main.js
+++ main.js
@@ -1,5 +1,8 @@
+// need to make query selectors to target the form input fields and they should be placed in global scope
 
-let books = [book 1, book 2, etc];
+
+
+let books = [];
 
 // when someon submits the form with the inputs that correspond the parameters below
 // the constructor needs to run in order to make a new Book object
@@ -11,27 +14,31 @@ function Book(title, author, pages, read){
 }
 
 Book.prototype.readStatus() {
-  
+
 };
 
 function saveInput() {
   // save user input AS AN OBJECT, probably from a form, and add it into the book array
   // the newBookVariable is where we save our new Book object after a user inputs arguments
   books.push(newBookVariable);
+
+  //take the input value and store it in the corresponding fields 
+
 }
 
 // function that loops through the array and displays each book on the homepage
+// might I need to use a standard 'for' loop? the forEach() loop does NOT return a value
 function displayArray(booksArray) {
   booksArray.forEach((element) => {
     //create a new card
       //create div with text fields for title, author, pages, and read
-      const bookDiv = document.createElement(`div[class="${element}"]`);
+      const bookDiv = document.createElement('div').classList.add(`${element}`);
       //can I give it a class or an ID? check if I gave it a class correctly
 
       //dealing with the first book object here
 
-      const bookTitle = document.createElement(`p[class="${element}"]`)
-      bookTitle.textContent = `Title: ${element.title}`
+      const bookTitle = document.createElement('p').classList.add(`${element}`);
+      bookTitle.textContent = `Title: ${element.title}`;
       //Create new elements for author, pages, and read and then add text content.
 
       //append all the new <p> elements to the new div
@@ -41,7 +48,7 @@ function displayArray(booksArray) {
       //add a button element to the card to remove the book from the library
       //add a buttom element to the card to change the books 'read' status
   }
-}
+})
 
 function removeCard() {
   //event listener to know if the button in the card was toggled
